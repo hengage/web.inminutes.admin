fce52cca00916860b241d69de99dbaec63531e42
sikiru <sikirurazak1@gmail.com>
2025-03-02 13:02:14 +0100

    update custom button component

--- src/components/ui/custom/button.tsx
+++ src/components/ui/custom/button.tsx
@@ -1,6 +1,7 @@
 import { Button, ButtonProps } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { VariantProps, cva } from "class-variance-authority";
+import React from "react";
 
 const buttonVariants = cva(
   "px-4 py-2 font-normal !pointer-events-auto  hover:disabled:cursor-not-allowed",
@@ -67,24 +68,39 @@ export function CustomButton({
 }: CustomButtonProps) {
   const render = loading ? (
     <span className={cn(spinVariants({ variant: selectSpinVariants(variant), size }))}></span>
+  ) : React.isValidElement(children) ? (
+    React.cloneElement(
+      children as React.ReactElement<HTMLElement>,
+      {
+        className: cn("relative"),
+      },
+      <>
+        {/* Predefined Children */}
+        {slotBefore && !loading && (
+          <div className="absolute inset-y-0 left-0 flex items-center pl-3">{slotBefore}</div>
+        )}
+        {React.Children.map(children, (child) =>
+          typeof child === "string"
+            ? child
+            : React.isValidElement(child)
+              ? ((child.props as { children?: React.ReactNode }).children ?? "")
+              : ""
+        )?.join("") ?? ""}
+        {slotAfter && !loading && (
+          <div className="absolute inset-y-0 right-0 flex items-center pr-3">{slotAfter}</div>
+        )}
+      </>
+    )
   ) : (
     children
   );
   return (
-    <div className="relative w-full">
-      {slotBefore && !loading && (
-        <div className="absolute inset-y-0 left-0 flex items-center pl-3">{slotBefore}</div>
-      )}
-      <Button
-        className={cn(buttonVariants({ variant, size, className }))}
-        {...props}
-        variant={"child"}
-      >
-        {render}
-      </Button>
-      {slotAfter && !loading && (
-        <div className="absolute inset-y-0 right-0 flex items-center pr-3">{slotAfter}</div>
-      )}
-    </div>
+    <Button
+      className={cn(buttonVariants({ variant, size, className }))}
+      {...props}
+      variant={"child"}
+    >
+      {render}
+    </Button>
   );
 }
