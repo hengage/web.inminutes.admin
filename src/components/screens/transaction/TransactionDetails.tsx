f00dd1d1d5ec64072a57616d734208f58aec3ede
Your Name <roheemohmudashir@gmail.com>
2025-05-09 13:09:39 +0100

    fix(transaction details)

--- src/components/screens/transaction/TransactionDetails.tsx
+++ src/components/screens/transaction/TransactionDetails.tsx
@@ -1,24 +1,17 @@
 "use client";
-
 import React from "react";
 import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
-import { Button } from "@/components/ui/button";
 import { useGetTransactionByIdQuery } from "@/api/transaction";
+import Tag from "@/components/general/Tag";
+import { tag } from "@/types";
 
 interface TransactionDetailsProps {
-  transactionId: string;
+  transaction: string;
   children: React.ReactNode;
 }
 
-const TransactionDetails = ({ transactionId, children }: TransactionDetailsProps) => {
-  const { data: transaction, isLoading } = useGetTransactionByIdQuery(transactionId);
-  console.log(transaction?.data);
-
-  // const { data, isLoading } = useGetProductByIdQuery(id);
-  // if (isLoading) return <div>Loading...</div>;
-  // const product = data;
-  // console.log(product);
-  // if (!product) return <div>No product found</div>;
+const TransactionDetails = ({ transaction, children }: TransactionDetailsProps) => {
+  const { data: transactionData, isLoading } = useGetTransactionByIdQuery(transaction);
 
   const formatDate = (dateString: string) => {
     const date = new Date(dateString);
@@ -46,38 +39,32 @@ const TransactionDetails = ({ transactionId, children }: TransactionDetailsProps
           <div className="flex items-center justify-center h-64">
             <div className="animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-primary"></div>
           </div>
-        ) : transaction ? (
-          <div className="p-6 overflow-y-auto">
+        ) : transactionData ? (
+          <div className="py-4 overflow-y-auto">
             <div className="mb-4">
-              <h3 className="text-sm text-gray-500 mb-1">ID: #{transaction.reference}</h3>
-              {transaction.status.toLowerCase() === "successful" ? (
-                <div className="bg-green-100 text-green-600 px-3 py-1 rounded-full text-sm inline-flex items-center">
-                  <span className="h-2 w-2 bg-green-600 rounded-full mr-2"></span>
-                  Successful
-                </div>
-              ) : (
-                <div className="bg-red-100 text-red-600 px-3 py-1 rounded-full text-sm inline-flex items-center">
-                  <span className="h-2 w-2 bg-red-600 rounded-full mr-2"></span>
-                  Failed
-                </div>
-              )}
+              <h3 className="text-sm text-gray-500 mb-1">ID: #{transactionData.reference}</h3>
+              <Tag tag={transactionData.status.toLowerCase() as tag} />
             </div>
 
             {/* Transaction Reason */}
             <div className="mb-4 border rounded-lg p-4 bg-gray-50">
               <h3 className="text-sm text-gray-500 mb-1">Transaction Reason</h3>
-              <p className="font-medium">{transaction.reason}</p>
+              <p className="font-medium">{transactionData.reason}</p>
             </div>
 
             {/* Details */}
             <div className="mb-4 border rounded-lg p-4 bg-gray-50">
               {[
-                ["Order ID", transaction._id],
-                ["Merchant Code", `MCH_${transaction.reference.slice(0, 8)}`],
-                ["Transfer Code", "TRF"],
-                ["Amount", `₦${transaction.amount}`],
-                ["Processing Fee", "₦50"],
-                ["Status", transaction.status],
+                ["Order ID", transactionData._id],
+                ["Merchant Code", `MCH_${transactionData.reference.slice(0, 8)}`],
+                ["Transfer Code", transactionData.transferCode],
+                ["Amount", `₦${transactionData.amount}`],
+                ["Processing Fee", `₦${transactionData.transactionFee}`],
+                ["Status", transactionData.status],
+                ["Bank Name", transactionData.bankName],
+                ["Account Name", transactionData.accountName],
+                ["Account Number", transactionData.accountNumber],
+                ["Currency", transactionData.currency],
               ].map(([label, value]) => (
                 <div key={label} className="flex justify-between items-center mb-3">
                   <h3 className="text-sm text-gray-500">{label}</h3>
@@ -90,19 +77,19 @@ const TransactionDetails = ({ transactionId, children }: TransactionDetailsProps
             <div className="mb-6 border rounded-lg p-4 bg-gray-50">
               <div className="flex justify-between items-center mb-3">
                 <h3 className="text-sm text-gray-500">Date</h3>
-                <p className="font-medium">{formatDate(transaction.createdAt).split(",")[0]}</p>
+                <p className="font-medium">{formatDate(transactionData.createdAt).split(",")[0]}</p>
               </div>
               <div className="flex justify-between items-center">
                 <h3 className="text-sm text-gray-500">Time</h3>
                 <p className="font-medium">
-                  {formatDate(transaction.createdAt).split(",")[1].trim()}
+                  {formatDate(transactionData.createdAt).split(",")[1].trim()}
                 </p>
               </div>
             </div>
-
+            {/* 
             <Button className="w-full" variant="destructive">
               Delete
-            </Button>
+            </Button> */}
           </div>
         ) : (
           <div className="p-6 text-center text-gray-500">Transaction not found</div>
