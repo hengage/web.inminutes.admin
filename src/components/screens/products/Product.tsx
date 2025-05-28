d3718cf8a135efee3124a652ee681f831ec036c6
Your Name <roheemohmudashir@gmail.com>
2025-05-28 15:24:34 +0100

    fix url params

--- src/components/screens/products/Product.tsx
+++ src/components/screens/products/Product.tsx
@@ -7,21 +7,19 @@ import ReviewProduct from "./ReviewProduct";
 import { Icon } from "@/components/ui/Icon";
 import CreateCategoryModal from "./CreateCategory";
 import { CustomButton as Button } from "@/components/ui/custom/button";
+import Link from "next/link";
+import useUrlHash from "@/hooks/useUrlHash";
 
 const Product = () => {
-  const [activeTab, setActiveTab] = useState<string>("0");
   const [openCategoryModal, setOpenCategoryModal] = useState(false);
-
-  const handleTabChange = (value: string) => {
-    setActiveTab(value);
-  };
+  const { hashParams, updateHashUrl } = useUrlHash();
 
   return (
     <div className="bg-white rounded-lg p-4 w-full flex flex-col">
       <div className="flex items-center justify-between mb-4">
         <h1 className="text-2xl font-bold mb-4 text-[#160A62]">Products</h1>
 
-        {activeTab === "1" && (
+        {hashParams.tab === "1" && (
           <Button
             variant="ctm-outline"
             slotBefore={<Icon name="add" height={16} width={16} />}
@@ -34,37 +32,58 @@ const Product = () => {
 
       <div>
         <Tab
-          value={activeTab}
-          onValueChange={handleTabChange}
           items={[
             {
               trigger: (
-                <button className="px-4 py-2 text-sm font-medium hover:text-indigo-700">
+                <Link
+                  href={{
+                    pathname: "/product",
+                    query: { page: 1, limit: 25 },
+                    hash: updateHashUrl({ tab: "0" }).substring(1),
+                  }}
+                  className="px-4 py-2 text-sm font-medium hover:text-indigo-700"
+                >
                   All Products
-                </button>
+                </Link>
               ),
+
               content: <AllProduct />,
               key: "0",
             },
             {
               trigger: (
-                <button className="px-4 py-2 text-sm font-medium hover:text-indigo-700">
+                <Link
+                  href={{
+                    pathname: "/product",
+                    query: { page: 1, limit: 25 },
+                    hash: updateHashUrl({ tab: "1" }).substring(1),
+                  }}
+                  className="px-4 py-2 text-sm font-medium hover:text-indigo-700"
+                >
                   Product Category
-                </button>
+                </Link>
               ),
               content: <ProductCategory />,
               key: "1",
             },
             {
               trigger: (
-                <button className="px-4 py-2 text-sm font-medium hover:text-indigo-700">
+                <Link
+                  href={{
+                    pathname: "/product",
+                    query: { page: 1, limit: 25 },
+                    hash: updateHashUrl({ tab: "2" }).substring(1),
+                  }}
+                  className="px-4 py-2 text-sm font-medium hover:text-indigo-700"
+                >
                   Reviewing Products
-                </button>
+                </Link>
               ),
               content: <ReviewProduct />,
               key: "2",
             },
           ]}
+          value={hashParams.tab ?? "0"}
         />
       </div>
 
